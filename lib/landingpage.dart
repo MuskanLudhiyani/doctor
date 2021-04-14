83416ac40423e6ff20b5e2091e1768262fea1399
muskan <muskanludhiyani@gmail.com>
2021-04-14 20:38:34 +0530

    signup is now happening

---
+++ lib/landingpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class landingpage extends StatefulWidget {
+  @override
+  _landingpageState createState() => _landingpageState();
+}
+
+class _landingpageState extends State<landingpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Here you come"),
+    );
+  }
+}

45bb6b985e3db0c75eccfa95954bc490d23a2479
muskan <muskanludhiyani@gmail.com>
2021-04-22 14:14:53 +0530

    ui updated signin

--- lib/screens/Sign_Up/signup.dart
+++ lib/screens/Sign_Up/signup.dart
@@ -6,111 +6,68 @@ class signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffEFF0F5),
+
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'Are you a Doctor or a Patient?',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 40,
-            ),
-          ),
+
           SizedBox(
             height: 25,
           ),
           Column(
             children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: Container(
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                        color: Colors.white,
-                        style: BorderStyle.solid,
-                        width: 1.0),
-                    color: Colors.transparent,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0xffF0EFFE),
-                        blurRadius: 2.0,
-                        spreadRadius: 0.0,
-                        offset:
-                        Offset(2.0, 2.0), // shadow direction: bottom right
-                      )
-                    ],
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      GestureDetector(
-                          child: Center(
-                            child: Text('Doctor',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    color: Color(0xff9994FA),
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Montserrat')),
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => (signupdoctor())),
-                            );
-                          })
-                    ],
+              GestureDetector(
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            "https://i.pinimg.com/originals/cf/4e/c0/cf4ec0f1cd566491fe962ed9d2f68d67.jpg"),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    height: 250,
+                    width: 250,
                   ),
                 ),
+
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => (signupdoctor())),
+                    );
+                  }
               ),
+
+
               SizedBox(
                 height: 10,
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: Container(
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                        color: Colors.white,
-                        style: BorderStyle.solid,
-                        width: 1.0),
-                    color: Colors.transparent,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0xffF0EFFE),
-                        blurRadius: 2.0,
-                        spreadRadius: 0.0,
-                        offset:
-                        Offset(2.0, 2.0), // shadow direction: bottom right
-                      )
-                    ],
+              GestureDetector(
+                  child: Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              "https://image.freepik.com/free-vector/sad-cute-kid-girl-cough-use-masker_97632-1577.jpg"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      height: 250,
+                      width: 250,
+                    ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      GestureDetector(
-                          child: Center(
-                            child: Text('Patient',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    color: Color(0xff9994FA),
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Montserrat')),
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => (signuppatient())),
-                            );
-                          })
-                    ],
-                  ),
-                ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => (signuppatient())),
+                    );
+                  }
               ),
             ],
           )
